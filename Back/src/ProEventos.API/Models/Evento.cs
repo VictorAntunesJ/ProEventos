d7a8e75bd6736cc330920418a765f986590be184
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-09-12 00:48:10 -0300

    Add Ef FrameworkCore

--- Back/src/ProEventos.API/Models/Evento.cs
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -11,8 +11,8 @@ namespace ProEventos.API.Models
         public string Local { get; set; }
         public string DataEvento { get; set; }
         public string Tema {get; set; }
-        public int  QtdPessoas { get; set; }
-        public string  Lote { get; set; }
-        public string  ImagemUrl { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemUrl { get; set; }
     }
 }
