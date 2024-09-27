54c33d42ed9b804d2fd5a0e6edc213284e80f953
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-09-27 00:24:01 -0300

    Add referencias (Request HTTP Angular)

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -3,28 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
+  public eventos: any;
 
-  public eventos: any = [
-  {
-    Tema:'Angular 11',
-    Local: 'Belo Horizonte'
-  },
-  {
-    Tema:'.Net 5',
-    Local: 'São Paulo'
-  },
-  {
-    Tema:'Angular e suas novidades',
-    Local: 'Rio de Janeiro'
+  constructor() {}
+
+  ngOnInit(): void {this.getEventos();}
+
+  public getEventos(): void {
+    this.eventos = [
+      {
+        Tema: 'Angular 11',
+        Local: 'Belo Horizonte',
+      },
+      {
+        Tema: '.Net 5',
+        Local: 'São Paulo',
+      },
+      {
+        Tema: 'Angular e suas novidades',
+        Local: 'Rio de Janeiro',
+      },
+    ];
   }
-]
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
+
+// = [
+// {
+//   Tema:'Angular 11',
+//   Local: 'Belo Horizonte'
+// },
+// {
+//   Tema:'.Net 5',
+//   Local: 'São Paulo'
+// },
+// {
+//   Tema:'Angular e suas novidades',
+//   Local: 'Rio de Janeiro'
+// }
+// ]
