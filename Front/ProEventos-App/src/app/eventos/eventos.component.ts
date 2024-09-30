ff8e7ec26f28da211549e0597e980099d5d02d9f
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-09-29 21:59:16 -0300

    A diretiva NGIF - Condição para o usuario

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
 
   constructor(private http: HttpClient) {}
 
@@ -16,10 +16,10 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => (this.eventos = response),
-      (error) => console.log(error)
-    );
+    // this.http.get('https://localhost:5001/api/eventos').subscribe(
+    //   (response) => (this.eventos = response),
+    //   (error) => console.log(error)
+    // );
   }
 }
 
