77092a6d70111399ae1c924534ff5736d683e096
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-09-30 22:22:07 -0300

    Propriedade Binding - Part02 - Vinculação de Dados.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public eventos: any = [];
-  widthImg: number = 100;
-  marginImg: number = 2;
+  larguraImagem: number = 150;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  alterarImagem()
+  {
+    this.exibirImagem = !this.exibirImagem;
   }
 
   public getEventos(): void {
