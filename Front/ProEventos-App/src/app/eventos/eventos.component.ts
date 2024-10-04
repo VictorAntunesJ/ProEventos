9995cdd4116cdee10d547fe095dd4526e4ca55b8
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-10-04 00:08:27 -0300

    Implementação de filtragem na grid de eventos - acertos alguns  metodos.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public eventos: any = [];
+  public eventosFiltrados: any = [];
   larguraImagem: number = 150;
   margemImagem: number = 2;
   exibirImagem: boolean = true;
-  filtroLista: string='';
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    // Corrigido aqui
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { local: any; tema: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -19,30 +41,33 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-  alterarImagem()
-  {
+  alterarImagem() {
     this.exibirImagem = !this.exibirImagem;
   }
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+
       (error) => console.log(error)
     );
   }
 }
 
-// = [
-// {
-//   Tema:'Angular 11',
-//   Local: 'Belo Horizonte'
-// },
-// {
-//   Tema:'.Net 5',
-//   Local: 'São Paulo'
-// },
-// {
-//   Tema:'Angular e suas novidades',
-//   Local: 'Rio de Janeiro'
-// }
-// ]
+// // = [
+// // {
+// //   Tema:'Angular 11',
+// //   Local: 'Belo Horizonte'
+// // },
+// // {
+// //   Tema:'.Net 5',
+// //   Local: 'São Paulo'
+// // },
+// // {
+// //   Tema:'Angular e suas novidades',
+// //   Local: 'Rio de Janeiro'
+// // }
+// // ]
