de65a5ee63049803a8c5176b1e24caa3bbc4799e
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-10-24 23:22:24 -0300

    Evento Application - Implementa métodos de CRUD de eventos com tratamento de exceções e mensagens personalizadas

--- Back/src/ProEventos.Application/Contratos/IEventoService.cs
+++ Back/src/ProEventos.Application/Contratos/IEventoService.cs
@@ -12,5 +12,10 @@ namespace ProEventos.Application.Contratos
         Task<Evento> UpDateEvento (int eventoId, Evento model);
         Task<bool> DeleteEvento (int eventoId);
 
+        
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
+        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes = false);
+
     }
 }
